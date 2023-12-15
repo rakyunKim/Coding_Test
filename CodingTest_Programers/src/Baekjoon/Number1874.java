b0a359b94ee15551fc62000d29a9d2fe66dc068e
rakyun1 <dbskim234@naver.com>
2023-12-15 01:19:31 -0700

    백준 1874번 complete

---
+++ CodingTest_Programers/src/Baekjoon/Number1874.java
@@ -0,0 +1,45 @@
+package Baekjoon;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Number1874 {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		final int ARR_LENGTH = scanner.nextInt();
+		final int[] ARR = new int[ARR_LENGTH];
+		boolean result = true;
+		for (int i = 0; i < ARR_LENGTH; i++){
+			ARR[i] = scanner.nextInt();
+		}
+		Stack<Integer> stack = new Stack<>();
+		List<String> resultList = new ArrayList<>();
+		int number = 1;
+		for (int i = 0; i < ARR_LENGTH; i++){
+			int currentNumber = ARR[i];
+			if (currentNumber >= number){
+				while (currentNumber >= number){
+					stack.push(number++);
+					resultList.add("+");
+				}
+				stack.pop();
+				resultList.add("-");
+			}else {
+				int current = stack.pop();
+				if (current > number){
+					System.out.println("NO");
+					result = false;
+					break;
+				}else {
+					resultList.add("-");
+				}
+			}
+		}
+		if (result){
+			resultList.forEach(System.out::println);
+		}
+	}
+}
