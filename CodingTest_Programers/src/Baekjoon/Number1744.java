c2fe2d61080c4331a44e84dae23ec5213ca358e9
rakyun1 <dbskim234@naver.com>
2023-12-22 21:40:55 -0700

    백준 1744번 complete(final)

--- CodingTest_Programers/src/Baekjoon/Number1744.java
+++ CodingTest_Programers/src/Baekjoon/Number1744.java
@@ -1,54 +1,71 @@
 package Baekjoon;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Number1744 {
+	static int zeroCount;
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 카드 묶음의 수 저장
-		// 양수는 내림차순 정렬
-		PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder());
-		PriorityQueue<Integer> minusPq = new PriorityQueue<>();
-		int one = 0;
-		int zero = 0;
-		for (int i = 0; i < N; i++) { // 4개의 그룹으로 분리하여 저장
-			int data = sc.nextInt();
-			if (data > 1) {
-				plusPq.add(data);
-			} else if (data == 1) {
-				one++;
-			} else if (data == 0) {
-				zero++;
+		Scanner scanner = new Scanner(System.in);
+		int numberCount = scanner.nextInt();
+		PriorityQueue<Integer> plus = new PriorityQueue<>(Comparator.reverseOrder());
+		PriorityQueue<Integer> minus = new PriorityQueue<>();
+		zeroCount = 0;
+		for (int i = 0; i < numberCount; i++){
+			int number = scanner.nextInt();
+			if (number > 0){
+				plus.add(number);
+			} else if (number < 0) {
+				minus.add(number);
 			} else {
-				minusPq.add(data);
+				zeroCount++;
 			}
 		}
-		int sum = 0;
-		// 양수처리
-		while (plusPq.size() > 1) {
-			int first = plusPq.remove();
-			int second = plusPq.remove();
-			sum = sum + first * second;
+
+		int minusTotal = checkMinus(minus);
+		int plusTotal = checkPlus(plus);
+		System.out.println(minusTotal + plusTotal);
+	}
+	private static int checkMinus(PriorityQueue<Integer> minus) {
+		int total = 0;
+
+		while (!minus.isEmpty()){
+			if (minus.size() == 1){
+				if (zeroCount != 0){
+					break;
+				}
+				total += minus.poll();
+				break;
+			}
+			int first = minus.poll();
+			int second = minus.poll();
+			total += first * second;
 		}
-		if (!plusPq.isEmpty()) {
-			sum = sum + plusPq.remove();
-		}
-		// 음수처리
-		while (minusPq.size() > 1) {
-			int first = minusPq.remove();
-			int second = minusPq.remove();
-			sum = sum + first * second;
-		}
-		if (!minusPq.isEmpty()) {
-			if (zero == 0) {
-				sum = sum + minusPq.remove();
+
+		return total;
+	}
+	private static int checkPlus(PriorityQueue<Integer> plus) {
+		int total = 0;
+
+		while (!plus.isEmpty()){
+			if (plus.size() == 1){
+				total += plus.poll();
+				break;
+			}
+			if (plus.peek() == 1){
+				total += plus.poll();
+			} else {
+				int first = plus.poll();
+				if (plus.peek() == 1){
+					total += first + plus.poll();
+				}else {
+					total += first * plus.poll();
+				}
 			}
 		}
-		// 1처리
-		sum = sum + one;
-		System.out.println(sum);
+
+		return total;
 	}
 
 }
