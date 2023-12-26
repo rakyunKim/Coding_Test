306be376ec940a30e21ed7f82121697ce499517a
rakyun1 <dbskim234@naver.com>
2023-12-25 19:14:43 -0700

    백준 1850번 complete

---
+++ CodingTest_Programers/src/Baekjoon/Number1850.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Number1850 {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		long firstNum = scanner.nextLong();
+		long secondNum = scanner.nextLong();
+
+		long result = 0;
+		if (firstNum > secondNum){
+			result = gcd(firstNum, secondNum);
+		}else {
+			result = gcd(secondNum, firstNum);
+		}
+
+		while (result > 0){
+			bufferedWriter.write("1");
+			result--;
+		}
+
+		bufferedWriter.flush();
+		bufferedWriter.close();
+	}
+	private static long gcd(long large, long small){
+		if (small == 0){
+			return large;
+		} else {
+			return gcd(small, large % small);
+		}
+	}
+}
