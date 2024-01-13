731e1f295863661eecf5ba6a15b3d2ec616d3b74
rakyun1 <dbskim234@naver.com>
2024-01-13 16:08:29 +0900

    백준 14501번 must try again

---
+++ CodingTest_Programers/src/Baekjoon/동적계획법/Number14501.java
@@ -0,0 +1,28 @@
+package Baekjoon.동적계획법;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Number14501 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] D = new int[N+2]; //오늘부터 퇴사일까지 벌수있는 최대 수입
+		int[] T = new int[N+1];
+		int[] P = new int[N+1];
+		for(int i=1; i<=N; i++)
+		{
+			T[i] = sc.nextInt();
+			P[i] = sc.nextInt();
+		}
+		for(int i=N;i>0;i--) {
+			if(i+T[i]>N+1) {
+				D[i]=D[i+1];
+			}
+			else {
+				D[i]=Math.max(D[i+1],P[i]+D[i+T[i]]);
+			}
+		}
+		System.out.println(D[1]);
+	}
+}
