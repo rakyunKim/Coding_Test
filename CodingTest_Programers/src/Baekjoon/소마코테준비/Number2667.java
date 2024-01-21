161eb0202ba4a531a39441e229d2eb4920763b7d
rakyun1 <dbskim234@naver.com>
2024-01-21 20:26:48 +0900

    Number2667 complete

--- CodingTest_Programers/src/Baekjoon/소마코테준비/Number2667.java
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number2667.java
@@ -2,46 +2,61 @@ package Baekjoon.소마코테준비;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Number2667 {
     static int[] dx = {0,1,0,-1};
     static int[] dy = {1,0,-1,0};
     static int[][] map;
-    static boolean[][] visited;
+    static int[][] visited;
     static int mapSize;
+    static List<Integer> houseCount;
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         mapSize = Integer.parseInt(bufferedReader.readLine());
-
+        houseCount = new ArrayList<>();
         map = new int[mapSize][mapSize];
+        visited = new int[mapSize][mapSize];
         for (int i = 0; i < mapSize; i++){
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            String temp = stringTokenizer.nextToken();
+            char[] tempArr = temp.toCharArray();
             for (int j = 0; j < mapSize; j++){
-                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                map[i][j] = Integer.parseInt(String.valueOf(tempArr[j]));
             }
         }
+
+        for (int i = 0; i < mapSize; i++){
+            for (int j = 0; j < mapSize; j++){
+                if (map[i][j] == 1){
+                    BFS(i, j);
+                }
+            }
+        }
+        System.out.println(houseCount.size());
+        houseCount.forEach(System.out::println);
     }
 
-    private static void BFS(int startY, int startX) {
+    private static void BFS(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {startY,startX});
-        visited[startY][startX] = true;
+        queue.offer(new int[] {i,j});
+        int count = 0;
         while (!queue.isEmpty()){
             int[] now = queue.poll();
             for (int k = 0; k < 4; k++){
                 int y = now[0] + dy[k];
                 int x = now[1] + dx[k];
                 if (y >= 0 && x >= 0 && y < mapSize && x < mapSize){ // 배열의 값을 넘어가면 안 되고
-                    if (map[y][x] != 0 && !visited[y][x]){ // 값이 0이어서 못가는 배열이거나 이미 방문한 배열일 경우
-                        visited[y][x] = true;
-                        map[y][x] = map[now[0]][now[1]] + 1; // 핵심
+                    if (map[y][x] == 1 && visited[y][x] == 0){ // 값이 0이어서 못가는 배열이거나 이미 방문한 배열일 경우
+                        visited[y][x] = 1;
                         queue.add(new int[] {y,x});
+                        count++;
                     }
                 }
             }
+        }
+        if (count != 0){
+            houseCount.add(count);
         }
     }
 }
