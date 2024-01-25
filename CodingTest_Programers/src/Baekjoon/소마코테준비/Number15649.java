4dbfce2b92d416f8b125361c86cada079eeb7c82
rakyun1 <dbskim234@naver.com>
2024-01-25 23:43:17 +0900

    Number1924 complete

---
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number15649.java
@@ -0,0 +1,16 @@
+package Baekjoon.소마코테준비;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Number15649 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int canUseNumberRange = scanner.nextInt();
+        int haveToUseNumberCount = scanner.nextInt();
+
+        for (int i = 0; i < canUseNumberRange; i++){
+            
+        }
+    }
+}
