3e1214dbf21adf4ff04767b57378438c70c47a3e
rakyun1 <dbskim234@naver.com>
2024-01-29 21:04:19 +0900

    Number11053 not yet

---
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number11053.java
@@ -0,0 +1,15 @@
+package Baekjoon.소마코테준비;
+
+import java.io.IOException;
+// util = Scanner, StringTokenizer
+import java.util.*;
+// io = BufferedReader, InputStreamReader
+import java.io.*;
+
+public class Number11053 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    }
+}
