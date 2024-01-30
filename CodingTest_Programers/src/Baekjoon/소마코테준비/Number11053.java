113b9bade0177e0f2d4b24057033935790b317b5
rakyun1 <dbskim234@naver.com>
2024-01-30 10:22:32 +0900

    Number11053 complete

--- CodingTest_Programers/src/Baekjoon/소마코테준비/Number11053.java
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number11053.java
@@ -9,7 +9,29 @@ import java.io.*;
 public class Number11053 {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int arrLength = scanner.nextInt();
+        int[] arr = new int[arrLength + 1];
+        arr[0] = 0;
+        for(int i = 1; i <= arrLength; i++){
+           arr[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[arrLength + 1];
+        dp[0] = 0;
+        for(int i = 1; i <= arrLength; i++){
+            for(int j = i -1; j >= 0; j--){
+                if(arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i : dp){
+            if(i > answer) {
+                answer = i;
+            }
+        }
+        System.out.println(answer);
     }
 }
