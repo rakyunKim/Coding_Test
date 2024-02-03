7d00601f2e705c95c246f2d4975f90ebda369490
rakyun1 <dbskim234@naver.com>
2024-02-03 23:17:17 +0900

    Number15988 complete

--- CodingTest_Programers/src/Baekjoon/소마코테준비/Number15988.java
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number15988.java
@@ -9,19 +9,13 @@ public class Number15988 {
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
+        for(int j = 4; j <= 1000000; j++){
+            dp[j] = (dp[j - 1] + dp[j - 2] + dp[j - 3]) % 1000000009;
+        }
         int testCase = scanner.nextInt();
         for(int i = 0; i < testCase; i++){
             int element = scanner.nextInt();
-            if(element == 1) System.out.println(1);
-            if(element == 2) System.out.println(2);
-            if(element == 3) System.out.println(4);
-
-            else{
-                for(int j = 4; j <= element; j++){
-                    dp[j] = (dp[j - 1] + dp[j - 2] + dp[j - 3]) % 1000000009;
-                }
-            }
-            System.out.println(dp[element] % 1000000009);
+            System.out.println(dp[element]);
         }
     }
 }
