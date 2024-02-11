1ecb6b8ca7d93467b777dde4e40d15e8baca5375
rakyun1 <dbskim234@naver.com>
2024-02-11 11:56:19 +0900

    Test

--- CodingTest_Programers/src/Baekjoon/소마코테준비/Practice.java
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Practice.java
@@ -1,4 +1,11 @@
 package Baekjoon.소마코테준비;
 
+import java.io.IOException;
+
 public class Practice {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("test");
+    }
+
 }
