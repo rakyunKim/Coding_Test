1e7d95a8142a7ecf0495db2781630dd3ead631e8
rakyun1 <dbskim234@naver.com>
2024-02-18 12:11:00 +0900

    Number11650 complete

---
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number11650.java
@@ -0,0 +1,34 @@
+package Baekjoon.소마코테준비;
+
+import java.io.IOException;
+import java.util.*;
+public class Number11650 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int pointCount = scanner.nextInt();
+        ArrayList<Integer>[] adjacency = new ArrayList[pointCount];
+        for(int i = 0; i < pointCount; i++){
+            adjacency[i] = new ArrayList<>();
+        }
+        int[][] point = new int[pointCount][2];
+
+        for(int i = 0; i < pointCount; i++){
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            point[i][0] = x;
+            point[i][1] = y;
+        }
+
+        Arrays.sort(point, (point1, point2) -> {
+           if(point1[0] == point2[0]) return point1[1] - point2[1];
+           else return point1[0] - point2[0];
+        });
+
+        for(int i = 0; i < pointCount; i++){
+            System.out.println(point[i][0] + " " + point[i][1]);
+        }
+    }
+}
