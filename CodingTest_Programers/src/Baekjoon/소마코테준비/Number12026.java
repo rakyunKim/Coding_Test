9f97c4cbc6252d09ab5d7fd561dd5e097ba46906
rakyun1 <dbskim234@naver.com>
2024-02-23 10:39:59 +0900

    Number12026 complete

--- CodingTest_Programers/src/Baekjoon/소마코테준비/Number12026.java
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number12026.java
@@ -1,51 +1,62 @@
-//package Baekjoon.소마코테준비;
-//import java.io.IOException;
-//import java.util.*;
-//
-//public class Number12026 {
-//    static int targetSize;
-//    static int[] arr;
-//    static int answer;
-//    public static void main(String[] args) throws IOException {
-//        Scanner scanner = new Scanner(System.in);
-//
-//
-//        targetSize = scanner.nextInt();
-//        arr = new int[targetSize];
-//
-//        String boj = scanner.next();
-//        char[] carr = new char[boj.length()];
-//
-//        for (int i = 0; i < boj.length(); i++) {
-//            carr[i] = boj.charAt(i);
-//            if(carr[i] == 'B') arr[i] = 1;
-//            else if(carr[i] == 'O') arr[i] = 2;
-//            else if(carr[i] == 'J') arr[i] = 3;
-//        }
-//
-//        answer = 1000000000;
-//
-////        for(int i = 1; i < targetSize; i++){
-////            if(arr[i] == 2){
-////                recursive(i, 3, i * i);
-////            }
-////        }
-//
-//        recursive(0, 2, 0);
-//
-//        System.out.println(answer == 1000000000 ? -1 : answer);
-//    }
-//
-//    private static void recursive(int idx, int target, int sum) {
-//        if(idx == targetSize - 1 && answer > sum){
-//            answer = sum;
-//        }
-//        for(int i = idx + 1; i < targetSize; i++){
-//            if(arr[i] == target){
-//                if(target == 1) recursive(i, 2, sum + ((i - idx) * (i - idx)));
-//                if(target == 2) recursive(i, 3, sum + ((i - idx) * (i - idx)));
-//                if(target == 3) recursive(i, 1, sum + ((i - idx) * (i - idx)));
-//            }
-//        }
-//    }
-//}
+package Baekjoon.소마코테준비;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Number12026 {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final int MAX = 987654321;
+    private static int N;
+    private static char[] street;
+    private static int[] dp;
+    private static void input() throws IOException {
+        N = Integer.parseInt(bufferedReader.readLine());
+        street = new char[N];
+        street = bufferedReader.readLine().toCharArray();
+        dp = new int[N];
+        Arrays.fill(dp, MAX);
+    }
+
+    private static void solve() throws IOException {
+        dp[0] = 0;
+        for(int i = 0; i < N-1; i++) {
+            int now = i;
+            char nowBlock = street[now];
+            if(nowBlock == 'B') {
+                for(int j = i +1; j < N; j++) {
+                    int next = j;
+                    char nextBlock = street[next];
+                    if(nextBlock == 'O') {
+                        dp[next] = Math.min(dp[next], dp[now] + (next-now) * (next-now));
+                    }
+                }
+            } else if(nowBlock == 'O') {
+                for(int j = i +1; j < N; j++) {
+                    int next = j;
+                    char nextBlock = street[next];
+                    if(nextBlock == 'J') {
+                        dp[next] = Math.min(dp[next], dp[now] + (next-now) * (next-now));
+                    }
+                }
+            } else {
+                for(int j = i +1; j < N; j++) {
+                    int next = j;
+                    char nextBlock = street[next];
+                    if(nextBlock == 'B') {
+                        dp[next] = Math.min(dp[next], dp[now] + (next-now) * (next-now));
+                    }
+                }
+            }
+        }
+        if(dp[N-1] == MAX) {
+            System.out.println("-1\n");
+        } else {
+            System.out.println(dp[N-1]);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+    }
+}
