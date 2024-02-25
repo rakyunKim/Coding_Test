8dea48354ea306ddbf97cd540759cb3ff74a63b3
rakyun1 <dbskim234@naver.com>
2024-02-25 12:13:35 +0900

    Number2309 complete

---
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number2309Retry.java
@@ -0,0 +1,66 @@
+package Baekjoon.소마코테준비;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Number2309Retry {
+    static int[] dwarfHeight;
+    static final int TARGET = 100;
+    static Stack<Integer> stack;
+    static int[] visited;
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        dwarfHeight = new int[9];
+        for(int i = 0; i < 9; i++){
+            dwarfHeight[i] = scanner.nextInt();
+        }
+        stack = new Stack<>();
+        visited = new int[9];
+        Arrays.sort(dwarfHeight);
+
+        find(0);
+
+    }
+
+    private static boolean find(int height) {
+//        System.out.println("현재 키 :: " + height);
+        if(stack.size() == 7){
+            if(height == TARGET){
+                Collections.sort(stack);
+                for(int i : stack){
+                    System.out.println(i);
+                }
+                System.out.println();
+                return true;
+            }
+            return false;
+        } else {
+            for(int i = 0; i < 9; i++){
+                if(visited[i] == 0){
+                    visited[i] = 1;
+                    stack.add(dwarfHeight[i]);
+                    height += dwarfHeight[i];
+                    if(find(height)){
+                        return true;
+                    } else {
+                        height -= dwarfHeight[i];
+                        stack.pop();
+                        visited[i] = 0;
+                    }
+                }
+            }
+            for(int i = 0; i < 9; i++){
+                if(visited[i] == 0){
+                    visited[i] = 1;
+                    if(find(height)){
+                        return true;
+                    } else {
+                        visited[i] = 0;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
