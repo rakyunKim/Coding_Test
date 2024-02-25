4939719cc847c6fdcecfd85df2189b403cebaf9c
rakyun1 <dbskim234@naver.com>
2024-02-25 12:32:18 +0900

    Number2309 complete

--- CodingTest_Programers/src/Baekjoon/소마코테준비/Number2309Retry.java
+++ CodingTest_Programers/src/Baekjoon/소마코테준비/Number2309Retry.java
@@ -23,7 +23,7 @@ public class Number2309Retry {
 
     }
 
-    private static boolean find(int height) {
+    private static void find(int height) {
 //        System.out.println("현재 키 :: " + height);
         if(stack.size() == 7){
             if(height == TARGET){
@@ -32,35 +32,71 @@ public class Number2309Retry {
                     System.out.println(i);
                 }
                 System.out.println();
-                return true;
+                System.exit(0);
             }
-            return false;
+            return ;
         } else {
             for(int i = 0; i < 9; i++){
                 if(visited[i] == 0){
                     visited[i] = 1;
                     stack.add(dwarfHeight[i]);
                     height += dwarfHeight[i];
-                    if(find(height)){
-                        return true;
-                    } else {
-                        height -= dwarfHeight[i];
-                        stack.pop();
-                        visited[i] = 0;
-                    }
+                    find(height);
+                    height -= dwarfHeight[i];
+                    stack.pop();
+                    visited[i] = 0;
+
                 }
             }
             for(int i = 0; i < 9; i++){
                 if(visited[i] == 0){
                     visited[i] = 1;
-                    if(find(height)){
-                        return true;
-                    } else {
-                        visited[i] = 0;
-                    }
+                    find(height);
+                    visited[i] = 0;
                 }
             }
         }
-        return false;
     }
+
+
+//    private static boolean find(int height) {
+////        System.out.println("현재 키 :: " + height);
+//        if(stack.size() == 7){
+//            if(height == TARGET){
+//                Collections.sort(stack);
+//                for(int i : stack){
+//                    System.out.println(i);
+//                }
+//                System.out.println();
+//                return true;
+//            }
+//            return false;
+//        } else {
+//            for(int i = 0; i < 9; i++){
+//                if(visited[i] == 0){
+//                    visited[i] = 1;
+//                    stack.add(dwarfHeight[i]);
+//                    height += dwarfHeight[i];
+//                    if(find(height)){
+//                        return true;
+//                    } else {
+//                        height -= dwarfHeight[i];
+//                        stack.pop();
+//                        visited[i] = 0;
+//                    }
+//                }
+//            }
+//            for(int i = 0; i < 9; i++){
+//                if(visited[i] == 0){
+//                    visited[i] = 1;
+//                    if(find(height)){
+//                        return true;
+//                    } else {
+//                        visited[i] = 0;
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
