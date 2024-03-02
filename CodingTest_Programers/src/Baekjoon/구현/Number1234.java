621c20b0f5cd9a1b69ce052083b2a06c6a6a0af9
rakyun1 <dbskim234@naver.com>
2024-03-02 23:35:33 +0900

    Number14500 complete

---
+++ CodingTest_Programers/src/Baekjoon/구현/Number1234.java
@@ -0,0 +1,10 @@
+package Baekjoon.구현;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Number1234 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
