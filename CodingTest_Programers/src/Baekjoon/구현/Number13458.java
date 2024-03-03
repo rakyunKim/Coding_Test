e316b8fd96259067079a6d125b9bceba91bf6bd1
rakyun1 <dbskim234@naver.com>
2024-03-03 10:05:04 +0900

    Number13458 ing

--- CodingTest_Programers/src/Baekjoon/구현/Number13458.java
+++ CodingTest_Programers/src/Baekjoon/구현/Number13458.java
@@ -3,9 +3,12 @@ package Baekjoon.구현;
 import java.io.IOException;
 import java.util.*;
 
-public class Number1234 {
+public class Number13458 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
+        int testRoomCount = scanner.nextInt();
+        int testerCount = scanner.nextInt();
+        int commander = scanner.nextInt();
+        int subCommander = scanner.nextInt();
     }
 }
