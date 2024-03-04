53250f0c124a1d6eb567cbb6a4d3361bbe6017a3
rakyun1 <dbskim234@naver.com>
2024-03-04 21:18:58 +0900

    Number13458 complete

--- CodingTest_Programers/src/Baekjoon/구현/Number13458.java
+++ CodingTest_Programers/src/Baekjoon/구현/Number13458.java
@@ -4,11 +4,33 @@ import java.io.IOException;
 import java.util.*;
 
 public class Number13458 {
+    static int commander;
+    static int subCommander;
     public static void main(String[] args) throws NumberFormatException, IOException {
         Scanner scanner = new Scanner(System.in);
         int testRoomCount = scanner.nextInt();
-        int testerCount = scanner.nextInt();
-        int commander = scanner.nextInt();
-        int subCommander = scanner.nextInt();
+
+        int[] room = new int[testRoomCount];
+        for(int i = 0; i < testRoomCount; i++){
+            room[i] = scanner.nextInt();
+        }
+
+        commander = scanner.nextInt();
+        subCommander = scanner.nextInt();
+
+        long totalSupervisor = testRoomCount;
+        for(int i = 0; i < testRoomCount; i++){
+            int studentCount = room[i];
+
+            int remainingStudent = studentCount - commander;
+
+            if(remainingStudent > 0){
+                totalSupervisor += remainingStudent % subCommander == 0 ?
+                        remainingStudent / subCommander :
+                        (remainingStudent / subCommander) + 1;
+            }
+        }
+
+        System.out.println(totalSupervisor);
     }
 }
