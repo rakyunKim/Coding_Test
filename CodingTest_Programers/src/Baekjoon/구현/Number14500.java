2107cfd58dafcb667f547f84c3fa8250e0485b37
rakyun1 <dbskim234@naver.com>
2024-03-06 06:49:15 +0900

    Number14500 retry ing

---
+++ CodingTest_Programers/src/Baekjoon/구현/Number14500.java
@@ -0,0 +1,26 @@
+package Baekjoon.구현;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Number14500 {
+
+    static int max = Integer.MIN_VALUE;
+    static int[][] arr;
+    static boolean[][] visit;
+    static int n;
+    static int m;
+
+    // 상하좌우
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(max);
+    }
+}
