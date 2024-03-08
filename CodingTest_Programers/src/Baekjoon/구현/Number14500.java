b0114a94c78a240ff790465f1f347536aeb28bee
rakyun1 <dbskim234@naver.com>
2024-03-08 22:58:46 +0900

    Number14500 retry ing

--- CodingTest_Programers/src/Baekjoon/구현/Number14500.java
+++ CodingTest_Programers/src/Baekjoon/구현/Number14500.java
@@ -7,10 +7,10 @@ import java.util.StringTokenizer;
 public class Number14500 {
 
     static int max = Integer.MIN_VALUE;
-    static int[][] arr;
+    static int[][] map;
     static boolean[][] visit;
-    static int n;
-    static int m;
+    static int ySize;
+    static int xSize;
 
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
@@ -20,8 +20,68 @@ public class Number14500 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int ySize = Integer.parseInt(st.nextToken());
-        int xSize = Integer.parseInt(st.nextToken());
-        System.out.println(max);
+        ySize = Integer.parseInt(st.nextToken());
+        xSize = Integer.parseInt(st.nextToken());
+        map = new int[ySize][xSize];
+
+        for(int i = 0; i < ySize; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < xSize; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+//        for(int i = 0; i < ySize; i++){
+//            for(int j = 0; j < xSize; j++){
+//                System.out.print(map[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+        int first = findFirst();
+
+        int second = findSecond();
+        System.out.println(second);
+    }
+
+    private static int findSecond() {
+        int max = 0;
+
+        for(int i = 0; i + 1 < ySize; i++){
+            int sum = 0;
+            for(int j = 0; j + 1 < xSize; j++){
+                sum = map[i][j] + map[i][j  + 1]
+                        + map[i + 1][j] + map[i + 1][j + 1];
+                max = Math.max(sum, max);
+            }
+        }
+
+        return max;
+    }
+
+    private static int findFirst() {
+        int max = 0;
+
+        for(int i = 0; i < ySize; i++){
+            int xPointer = 0;
+            while(xPointer + 3 < xSize){
+                int sum = map[i][xPointer] + map[i][xPointer + 1]
+                        + map[i][xPointer + 2] + map[i][xPointer + 3];
+                max = Math.max(sum, max);
+                xPointer++;
+            }
+        }
+
+        for(int i = 0; i < xSize; i++){
+            int yPointer = 0;
+            while(yPointer + 3 < ySize){
+                int sum = map[yPointer][i] + map[yPointer + 1][i]
+                        + map[yPointer + 2][i] + map[yPointer + 3][i];
+                max = Math.max(sum, max);
+                yPointer++;
+            }
+        }
+
+        return max;
     }
 }
