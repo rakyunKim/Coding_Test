7bf052ff56319c5d4adb2e376ae38e9a4440897b
rakyun1 <dbskim234@naver.com>
2024-04-06 20:11:45 +0900

    카카오 기출문제 : 경주로 건설 init

---
+++ CodingTest_Programers/src/Programers/kakao/ConstructingRaceTrack.java
@@ -0,0 +1,19 @@
+package Programers.kakao;
+
+import java.io.IOException;
+
+public class ConstructingRaceTrack {
+    public static void main(String[] args) throws IOException {
+        int[][] board = new int[][]{
+                {0,0,0},
+                {0,0,0},
+                {0,0,0}
+        };
+
+        System.out.println(solution(board));
+    }
+    public static int solution(int[][] board) {
+        int answer = 0;
+        return answer;
+    }
+}
