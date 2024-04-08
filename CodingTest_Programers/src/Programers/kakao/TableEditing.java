0deba576f930293b54f51eb5bc6832e43743af68
rakyun1 <dbskim234@naver.com>
2024-04-08 11:06:09 +0900

    카카오 기출문제 : 표 편집 complete

---
+++ CodingTest_Programers/src/Programers/kakao/TableEditing.java
@@ -0,0 +1,50 @@
+package Programers.kakao;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class TableEditing {
+    public static void main(String[] args) throws IOException {
+        int n = 8;
+        int k = 2;
+
+        String[] cmd = new String[]{"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
+//        String[] cmd = new String[]{"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"};
+
+        System.out.println(solution(n, k, cmd));
+    }
+
+    public static String solution(int n, int k, String[] cmd) {
+        Stack<Integer> remove_order = new Stack<>();
+        int table_size = n;
+        for(int i=0; i<cmd.length; i++) {
+            char c = cmd[i].charAt(0);
+            if(c == 'D') {
+                k += Integer.parseInt(cmd[i].substring(2));
+            }
+            else if(c == 'U') {
+                k -= Integer.parseInt(cmd[i].substring(2));
+            }
+            else if(c == 'C') {
+                remove_order.add(k);
+                table_size--;
+                if(k == table_size)
+                    k--;
+            }
+            else if(c == 'Z') {
+                if(remove_order.pop()<=k)
+                    k++;
+                table_size++;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("O".repeat(Math.max(0, table_size)));
+
+        while(!remove_order.isEmpty())
+            builder.insert(remove_order.pop(), "X");
+
+        return builder.toString();
+    }
+}
