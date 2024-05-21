3ef0cc5eb672c1479adecf2ae5625d7500a83445
rakyun1 <dbskim234@naver.com>
2024-05-21 14:56:28 +0900

    백준 14500 complete

--- CodingTest_Programers/src/Baekjoon/구현/Number14500.java
+++ CodingTest_Programers/src/Baekjoon/구현/Number14500.java
@@ -1,87 +1,149 @@
 package Baekjoon.구현;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.util.*;
 
 public class Number14500 {
-
-    static int max = Integer.MIN_VALUE;
     static int[][] map;
-    static boolean[][] visit;
+    static int max = 0;
     static int ySize;
     static int xSize;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+        ySize = scanner.nextInt();
+        xSize = scanner.nextInt();
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        ySize = Integer.parseInt(st.nextToken());
-        xSize = Integer.parseInt(st.nextToken());
         map = new int[ySize][xSize];
 
-        for(int i = 0; i < ySize; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < xSize; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < ySize; i++) {
+            for(int j = 0; j < xSize; j++) {
+                map[i][j] = scanner.nextInt();
             }
         }
 
-//        for(int i = 0; i < ySize; i++){
-//            for(int j = 0; j < xSize; j++){
+        checkTetromino();
+
+        System.out.println(max);
+
+//        for(int i = 0; i < ySize; i++) {
+//            for(int j = 0; j < xSize; j++) {
 //                System.out.print(map[i][j]);
 //            }
 //            System.out.println();
 //        }
-
-        int first = findFirst();
-
-        int second = findSecond();
-        System.out.println(second);
     }
 
-    private static int findSecond() {
-        int max = 0;
+    private static void checkTetromino() {
+        int sum;
+        for(int i = 0; i < ySize; i++) {
+            for(int j = 0; j < xSize; j++) {
+                if(i + 2 < ySize && j + 1 < xSize) {
+                    // 1번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 2][j] + map[i + 2][j + 1];
+                    max = Math.max(max, sum);
 
-        for(int i = 0; i + 1 < ySize; i++){
-            int sum = 0;
-            for(int j = 0; j + 1 < xSize; j++){
-                sum = map[i][j] + map[i][j  + 1]
-                        + map[i + 1][j] + map[i + 1][j + 1];
-                max = Math.max(sum, max);
+                    // 3번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i + 1][j + 1] + map[i + 2][j + 1];
+                    max = Math.max(max, sum);
+
+                    // 2번의 1번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 1][j + 1] + map[i + 2][j + 1];
+                    max = Math.max(max, sum);
+
+                    // 3번의 2번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 1][j + 1] + map[i + 2][j];
+                    max = Math.max(max, sum);
+                }
+
+                if(i - 1 >= 0 && j + 2 < xSize) {
+                    // 2번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i][j + 2] + map[i - 1][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 4번
+                    sum = 0;
+                    sum += map[i][j] + map[i - 1][j] + map[i - 1][j + 1] + map[i - 1][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 2번의 2번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i - 1][j + 1] + map[i - 1][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 3번의 3번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i - 1][j + 1] + map[i][j + 2];
+                    max = Math.max(max, sum);
+                }
+
+                if(i + 2 < ySize && j - 1 >= 0) {
+                    // 5번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j - 1] + map[i + 1][j - 1] + map[i + 2][j - 1];
+                    max = Math.max(max, sum);
+
+                    // 7번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 2][j] + map[i + 2][j - 1];
+                    max = Math.max(max, sum);
+
+                    // 2번의 3번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 1][j - 1] + map[i + 2][j - 1];
+                    max = Math.max(max, sum);
+
+                    // 3번의 4번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 1][j - 1] + map[i + 2][j];
+                    max = Math.max(max, sum);
+                }
+
+                if(i + 1 < ySize && j + 2 < xSize) {
+                    // 6번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i][j + 2] + map[i + 1][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 8번
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 1][j + 1] + map[i + 1][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 2번의 4번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i + 1][j + 1] + map[i + 1][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 3번의 1번
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i + 1][j + 1] + map[i][j + 2];
+                    max = Math.max(max, sum);
+
+                    // 네모
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i + 1][j] + map[i + 1][j + 1];
+                    max = Math.max(max, sum);
+                }
+
+                // 막대기
+                if(j + 3 < xSize) {
+                    sum = 0;
+                    sum += map[i][j] + map[i][j + 1] + map[i][j + 2] + map[i][j + 3];
+                    max = Math.max(max, sum);
+                }
+
+                if(i + 3 < ySize) {
+                    sum = 0;
+                    sum += map[i][j] + map[i + 1][j] + map[i + 2][j] + map[i + 3][j];
+                    max = Math.max(max, sum);
+                }
+
             }
         }
-
-        return max;
-    }
-
-    private static int findFirst() {
-        int max = 0;
-
-        for(int i = 0; i < ySize; i++){
-            int xPointer = 0;
-            while(xPointer + 3 < xSize){
-                int sum = map[i][xPointer] + map[i][xPointer + 1]
-                        + map[i][xPointer + 2] + map[i][xPointer + 3];
-                max = Math.max(sum, max);
-                xPointer++;
-            }
-        }
-
-        for(int i = 0; i < xSize; i++){
-            int yPointer = 0;
-            while(yPointer + 3 < ySize){
-                int sum = map[yPointer][i] + map[yPointer + 1][i]
-                        + map[yPointer + 2][i] + map[yPointer + 3][i];
-                max = Math.max(sum, max);
-                yPointer++;
-            }
-        }
-
-        return max;
     }
 }
