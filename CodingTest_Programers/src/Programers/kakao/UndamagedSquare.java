3526a2642338b67969fa5f9da690cab2c4c21b82
rakyun1 <dbskim234@naver.com>
2024-05-25 17:44:54 +0900

    프로그래머스 카카오 기출 : 멀쩡한 사각형 not yet

---
+++ CodingTest_Programers/src/Programers/kakao/UndamagedSquare.java
@@ -0,0 +1,14 @@
+package Programers.kakao;
+
+import java.io.IOException;
+
+public class UndamagedSquare {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+    }
+
+    public static long solution(int w, int h) {
+        long answer = 1;
+        return answer;
+    }
+}
