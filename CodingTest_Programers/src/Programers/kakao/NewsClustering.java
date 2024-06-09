e7951f9085f86982cc8ddbb0fce9a92a6ae233af
rakyun1 <dbskim234@naver.com>
2024-06-09 16:13:51 +0900

    프로그래머스 카카오 기출 : 뉴스 클러스터링 uncompleted

---
+++ CodingTest_Programers/src/Programers/kakao/NewsClustering.java
@@ -0,0 +1,36 @@
+package Programers.kakao;
+
+import java.io.IOException;
+import java.util.*;
+
+public class NewsClustering {
+    public static void main(String[] args) throws IOException {
+        String str1 = "FRANCE";
+        String str2 = "french";
+
+//        String str1 = "handshake";
+//        String str2 = "shake hands";
+
+//        String str1 = "aa1+aa2";
+//        String str2 = "AAAA12";
+
+//        String str1 = "E=M*C^2";
+//        String str2 = "e=m*c^2";
+
+
+        System.out.println(solution(str1, str2));
+    }
+
+    public static int solution(String str1, String str2) {
+        List<String>
+
+        for (int i = 0; i < str1.length(); i++) {
+            while (Character.isLetter(str1.charAt(i))) {
+
+            }
+        }
+        System.out.println(str1.equalsIgnoreCase(str2));
+        int answer = 0;
+        return answer;
+    }
+}
